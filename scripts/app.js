42495f93158d528ffc74f52c5b4f7d4e36b3e1c0
Zekeriya <zekeriyakocairi@gmail.com>
2022-02-27 23:37:32 +0100

    add some functionality

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,11 @@ const urls = {
     search: '/search/movie',
 };
 
+window.onload = async function () {
+    let genresObj = await getGenres();
+    showGenres(genresObj.genres);
+}
+
 async function getData(relativeUrl) {
     let url = baseurl + relativeUrl;
     if (url.includes('?')) {
@@ -20,6 +25,26 @@ async function getGenres() {
     var response = await getData(urls.genres);
     var genres = await response.json();
     return genres;
+}
+
+async function selectGenre(genreName) { // search by genre
+    var searchResults = await searchByKeywordAsync(genreName);
+    showMovies(searchResults);
+}
+
+async function search() {
+    var searchText = document.getElementById('search').value;
+    var yearFilter = document.getElementById('year-filter').value;
+    var filterQuery = `&year=${yearFilter}`;
+    var results = await searchByKeywordAsync(searchText, filterQuery);
+    showMovies(results);
+}
+
+async function searchByKeywordAsync(searchText, filterQuery) {
+    var searchUrl = urls.search + `?query=${searchText}&include_adult=true${filterQuery}`;
+    var response = await getData(searchUrl);
+    var searchResults = await response.json();
+    return searchResults;
 }
 
 function showGenres(genres) {
@@ -42,16 +67,26 @@ function showGenres(genres) {
 
 }
 
-async function selectGenre(genreName) {
-    debugger;
-    var searchUrl = urls.search + `?query=${genreName}`;
-    var response = await getData(searchUrl);
-    var searchResults =  await response.json();
-    debugger;
+function showMovies({ results }) {
+    var cardsHtmlAggregate = results.reduce((acc, movie) => {
+        return acc + generateCardHtml(movie);
+    }, '');
 
+    document.getElementById('movie-cards').innerHTML = cardsHtmlAggregate;
 }
 
-window.onload = async function () {
-    let genresObj = await getGenres();
-    showGenres(genresObj.genres);
+
+function generateCardHtml(movie) {
+    movie.poster_path = movie.poster_path == null
+        ? 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png'
+        : 'https://image.tmdb.org/t/p/w200/' + movie.poster_path;
+    return `
+    <div class="movie-card">
+        <img src="${movie.poster_path}" alt="${movie.original_title}">
+        <div>
+            <h4>${movie.original_title}</h4>
+        </div>
+    </div>
+`;
 }
+
